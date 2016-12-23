621359378d9b910f48c380d3ba67e5c534e0bb97
Mark Lavrentyev <lavrema@outlook.com>
2016-12-23 17:10:48 -0500

    Make refresh clock tick down

--- Updater Script.js
+++ Updater Script.js
@@ -1,23 +1,15 @@
 var frcApiBaseLink = "https://frc-api.firstinspires.org/v2.0/";
 
-function countOneDown(sec) {
-    "use strict";
-    document.getElementById("refreshTime").innerHTML = sec;
-}
+var timeToRefresh = 30;
+
+
 function refreshSchedule() {
     "use strict";
 }
 
-function setCountdown() {
-    "use strict";
-    var sec;
-    for (sec = 30; sec > 0; sec -= 1) {
-        setTimeout(countOneDown(sec), 1000);
-    }
-    refreshSchedule();
+function refreshTimer() {
+	"use strict";
+	timeToRefresh -= 1;
+	document.getElementById("refreshTime").innerHTML = timeToRefresh;
+	setTimeout(refreshTimer, 1000);
 }
-
-window.onload = function start() {
-    "use strict";
-    setInterval(setCountdown(), 0);
-};
