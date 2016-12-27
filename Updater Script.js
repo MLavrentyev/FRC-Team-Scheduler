4221d7e4bfa3753834eab37ed1412cab339058d2
Mark Lavrentyev <lavrema@outlook.com>
2016-12-26 23:42:35 -0500

    Fix TBA event name request

--- Updater Script.js
+++ Updater Script.js
@@ -1,10 +1,10 @@
-var frcApiBaseLink = "https://frc-api.firstinspires.org/v2.0/";
-var TBAapiBaseLink = "https://thebluealliance.com/api/v2/";
+var frcApiBaseLink = "https://www.frc-api.firstinspires.org/v2.0/";
+var TBAapiBaseLink = "https://www.thebluealliance.com/api/v2/";
 var timeToRefresh = 30;
 
 var currentMatch = 1;
 var year = (new Date()).getFullYear();
-var eventKey = "CTWAT";
+var eventKey = "ctwat";
 var teamNumber = 4557;
 
 
@@ -12,7 +12,7 @@ function refreshSchedule() {
     "use strict";
 	var httpsRequest = new XMLHttpRequest();
 	
-	httpsRequest.open("GET", frcApiBaseLink + year + "/schedule/" + eventKey + "?tournamentLevel=&teamNumber=" + teamNumber + "&start=" + currentMatch, false);
+	httpsRequest.open("GET", frcApiBaseLink + year + "/schedule/" + eventKey + "?tournamentLevel=&teamNumber=" + teamNumber + "&start=" + currentMatch, true);
 	httpsRequest.setRequestHeader("Accept", "application/json");
 	httpsRequest.setRequestHeader("User-Agent", "");
 }
@@ -38,18 +38,24 @@ function setFootnoteInfo() {
 	document.getElementById("teamNumber").innerHTML = "FRC " + teamNumber;
 	var httpsRequest = new XMLHttpRequest(),
 		response;
-	
-	httpsRequest.open("GET", TBAapiBaseLink + "event/" + year + eventKey, false);
+	httpsRequest.open("GET", TBAapiBaseLink + "event/" + year + eventKey, true);
 	httpsRequest.setRequestHeader("X-TBA-App-Id", "mlavrentyev:liveMatchSchedule:v1");
-	httpsRequest.setRequestHeader("User-Agent", "liveScheduleUpdater");
-	httpsRequest.send(null);
+	
+	httpsRequest.send();
+	
+	httpsRequest.onreadystatechange = function (e) {
 
-	response = JSON.parse(httpsRequest);
-	document.getElementById("eventName").innerHTML = response.name;
+		if (this.readyState === XMLHttpRequest.DONE) {
+			
+			response = JSON.parse(httpsRequest.response);
+			document.getElementById("eventName").innerHTML = response.name;
+			console.log("All's good.");
+			
+		}
+	};
 }
 function startOnPageLoad() {
 	"use strict";
-	
 	setFootnoteInfo();
 	refreshTimer();
 }
