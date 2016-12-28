157401ea7c658ae7cdef9bb80af1eb7efaa1a12a
Mark Lavrentyev <lavrema@outlook.com>
2016-12-27 19:18:31 -0500

    Add method to get all of the events in a year

--- Main page/Updater Script.js
+++ Main page/Updater Script.js
@@ -1,5 +1,6 @@
 var frcApiBaseLink = "https://www.frc-api.firstinspires.org/v2.0/";
 var TBAapiBaseLink = "https://www.thebluealliance.com/api/v2/";
+var appID = "mlavrentyev:liveMatchSchedule:v1";
 var timeToRefresh = 30;
 
 var currentMatch = 1;
@@ -7,7 +8,7 @@ var year = (new Date()).getFullYear();
 var eventKey = "ctwat";
 var teamNumber = 4557;
 
-
+// Main page functions
 function refreshSchedule() {
     "use strict";
 	var httpsRequest = new XMLHttpRequest();
@@ -16,7 +17,6 @@ function refreshSchedule() {
 	httpsRequest.setRequestHeader("Accept", "application/json");
 	httpsRequest.setRequestHeader("User-Agent", "");
 }
-
 function refreshTimer() {
 	"use strict";
 	
@@ -39,7 +39,7 @@ function setFootnoteInfo() {
 	var httpsRequest = new XMLHttpRequest(),
 		response;
 	httpsRequest.open("GET", TBAapiBaseLink + "event/" + year + eventKey, true);
-	httpsRequest.setRequestHeader("X-TBA-App-Id", "mlavrentyev:liveMatchSchedule:v1");
+	httpsRequest.setRequestHeader("X-TBA-App-Id", appID);
 	
 	httpsRequest.send();
 	
@@ -54,8 +54,28 @@ function setFootnoteInfo() {
 		}
 	};
 }
-function startOnPageLoad() {
+function startOnMainPageLoad() {
 	"use strict";
 	setFootnoteInfo();
 	refreshTimer();
+}
+
+// Introduction page functions
+function getAllEvents() {
+	"use strict";
+	var allEventsRequest = new XMLHttpRequest(),
+		response;
+	allEventsRequest.open("GET", TBAapiBaseLink + "events/" + year);
+	allEventsRequest.setRequestHeader("X-TBA-App-Id", appID);
+	
+	allEventsRequest.send();
+	
+	allEventsRequest.onreadystatechange = function(e) {
+		if(this.readyState === XMLHttpRequest.DONE) {
+			response = JSON.parse(allEventsRequest.response);
+			for(var i = 0; i < response.length; i +=1) {
+				response[i].short_name;
+			}
+		}
+	}
 }
